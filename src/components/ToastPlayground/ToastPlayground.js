cfb4912c4e15d8203fc6758954bdddcaaac8c79b
Trevor Richardson <trevor@gaincompliance.com>
2023-10-12 12:34:27 -0500

    add toastshelf so multiple can be displayed. also allow form submission. right now not using unique keys for the toast list so could blow, but don't wanna add in random key indexing

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -4,13 +4,14 @@ import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
 import Toast from '../Toast';
+import ToastShelf from '../ToastShelf';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [variant, setVariant] = React.useState('notice');
   const [messageText, setMessageText] = React.useState();
-  const [showToast, setShowToast] = React.useState(false);
+  const [toasts, setToasts] = React.useState([]);
 
   return (
     <div className={styles.wrapper}>
@@ -19,54 +20,67 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
-      {showToast && <Toast variant={variant} onClose={() => setShowToast(false)}>{messageText}</Toast>}
+      {/*{showToast && <Toast variant={variant} onClose={() => setShowToast(false)}>{messageText}</Toast>}*/}
+      <form onSubmit={e => e.preventDefault()}>
+        <ToastShelf toasts={toasts} removeToast={indexToRemove => {
+          const newToasts = [...toasts];
+          newToasts.splice(indexToRemove, 1);
+          setToasts(newToasts);
+        }} />
 
-      <div className={styles.controlsWrapper}>
-        <div className={styles.row}>
-          <label
-            htmlFor="message"
-            className={styles.label}
-            style={{ alignSelf: 'baseline' }}
-          >
-            Message
-          </label>
-          <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} value={messageText} onChange={e => setMessageText(e.target.value)} />
+        <div className={styles.controlsWrapper}>
+          <div className={styles.row}>
+            <label
+              htmlFor="message"
+              className={styles.label}
+              style={{ alignSelf: 'baseline' }}
+            >
+              Message
+            </label>
+            <div className={styles.inputWrapper}>
+              <textarea id="message" className={styles.messageInput} value={messageText} onChange={e => setMessageText(e.target.value)} />
+            </div>
+          </div>
+
+          <div className={styles.row}>
+            <div className={styles.label}>Variant</div>
+            <div
+              className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+            >
+              {VARIANT_OPTIONS.map(option => (
+                <label key={option} htmlFor={`variant-${option}`}>
+                  <input
+                    id={`variant-${option}`}
+                    type="radio"
+                    name="variant"
+                    onClick={() => setVariant(option)}
+                    value={option}
+                    readOnly={true}
+                    checked={variant === option}
+                  />
+                  {option}
+                </label>
+              ))
+              }
+            </div>
+          </div>
+
+          <div className={styles.row}>
+            <div className={styles.label} />
+            <div
+              className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+            >
+              <Button onClick={event => {
+                event.preventDefault();
+
+                setToasts([...toasts, {variant, message: messageText}]);
+                setMessageText('');
+                setVariant('notice');
+              }}>Pop Toast!</Button>
+            </div>
           </div>
         </div>
-
-        <div className={styles.row}>
-          <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            {VARIANT_OPTIONS.map(option => (
-              <label key={option} htmlFor={`variant-${option}`}>
-                <input
-                  id={`variant-${option}`}
-                  type="radio"
-                  name="variant"
-                  onClick={() => setVariant(option)}
-                  value={option}
-                  readOnly={true}
-                  checked={variant === option}
-                />
-                {option}
-              </label>
-            ))
-            }
-          </div>
-        </div>
-
-        <div className={styles.row}>
-          <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <Button onClick={() => setShowToast(true)}>Pop Toast!</Button>
-          </div>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -3,15 +3,14 @@ import React from 'react';
 import Toast from '../Toast';
 import styles from './ToastShelf.module.css';
 
-function ToastShelf() {
+function ToastShelf({toasts = [], removeToast}) {
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+      {toasts.map((toast, index) => (
+        <li key={index} className={styles.toastWrapper}>
+          <Toast variant={toast.variant} onClose={() => removeToast(index)}>{toast.message}</Toast>
+        </li>
+      ))}
     </ol>
   );
 }
