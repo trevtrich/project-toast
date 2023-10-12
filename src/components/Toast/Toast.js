ad12c10a1fa533975ff5c725df4b8fa40b63049f
Trevor Richardson <trevor@gaincompliance.com>
2023-10-12 12:06:11 -0500

    allows popping the toast and changing the content inside

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -18,14 +18,16 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast() {
+function Toast({variant, children}) {
+  const VariantIcon = ICONS_BY_VARIANT[variant];
+
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
+    <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <VariantIcon size={24} />
       </div>
       <p className={styles.content}>
-        16 photos have been uploaded
+        {children}
       </p>
       <button className={styles.closeButton}>
         <X size={24} />
--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,11 +3,14 @@ import React from 'react';
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
+import Toast from '../Toast';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [variant, setVariant] = React.useState('notice');
+  const [messageText, setMessageText] = React.useState();
+  const [showToast, setShowToast] = React.useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -15,6 +18,8 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {showToast && <Toast variant={variant}>{messageText}</Toast>}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -26,7 +31,7 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea id="message" className={styles.messageInput} value={messageText} onChange={e => setMessageText(e.target.value)} />
           </div>
         </div>
 
@@ -36,13 +41,14 @@ function ToastPlayground() {
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
             {VARIANT_OPTIONS.map(option => (
-              <label htmlFor={`variant-${option}`}>
+              <label key={option} htmlFor={`variant-${option}`}>
                 <input
                   id={`variant-${option}`}
                   type="radio"
                   name="variant"
                   onClick={() => setVariant(option)}
                   value={option}
+                  readOnly={true}
                   checked={variant === option}
                 />
                 {option}
@@ -57,7 +63,7 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => setShowToast(true)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
